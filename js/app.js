2c596776f802dcb6fa411279af3180b0281b72db
Daniel Wood <daniel.93.wood@gmail.com>
2018-05-24 19:44:33 +0100

    print values from rgb sliders

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
+       
 var RGBChange = function() {
 	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
 };
 
+var PrintChanged = function() {
+    console.log('(R,G,B): (' + r.getValue()+', '+g.getValue()+', '+b.getValue()+')');
+};
+
 var r = $('#R').slider()
-		.on('slide', RGBChange)
-		.data('slider');
+            .on('slide', RGBChange)
+            .on('slide', PrintChanged)
+            .data('slider');
 var g = $('#G').slider()
-		.on('slide', RGBChange)
-		.data('slider');
+            .on('slide', RGBChange)
+            .on('slide', PrintChanged)
+            .data('slider');
 var b = $('#B').slider()
-		.on('slide', RGBChange)
-		.data('slider');
+            .on('slide', RGBChange)
+            .on('slide', PrintChanged)
+            .data('slider');
+        
+   
