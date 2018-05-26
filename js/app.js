cacb5a992edeeada2e46bdbfca88a9c333805d8f
Daniel Wood <daniel.93.wood@gmail.com>
2018-05-26 02:41:26 +0100

    custom slot button functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
        
-var RGBChange = function() {
+function RGBChange() {
 	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
 };
 
-var PrintChanged = function() {
+function PrintChanged() {
     console.log('(R,G,B): (' + r.getValue()+', '+g.getValue()+', '+b.getValue()+')');
 };
 
 function getBtnColour(rgb_colour){
     console.log('RGB Colour: ' + rgb_colour);
+}
 
+function getCustomSlot(slot_num){
+    console.log('Get colour from slot: ' + slot_num);
 }
 
 var r = $('#R').slider()
