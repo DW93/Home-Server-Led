d30cc35af8110d2d333f97b454063bf9929cb292
Daniel Wood <daniel.93.wood@gmail.com>
2018-05-27 03:11:18 +0100

    add button functionality

--- js/app.js
+++ js/app.js
@@ -7,12 +7,40 @@ function PrintChanged() {
     console.log('(R,G,B): (' + r.getValue()+', '+g.getValue()+', '+b.getValue()+')');
 };
 
-function getBtnColour(rgb_colour){
-    console.log('RGB Colour: ' + rgb_colour);
+function getBtnColour(rgb_val){
+    console.log('RGB Colour: ' + rgb_val);
 }
 
-function getCustomSlot(slot_num){
+function getCustomSlotColour(slot_num){
     console.log('Get colour from slot: ' + slot_num);
+    //get value
+}
+
+function saveColourToSlot(slot_num){
+    console.log('Save colour to slot: ' + slot_num);
+    //save value
+}
+
+//--------------------------------------------------------
+// effects
+function rainbowEffect(){
+    console.log('Rainbow effect');
+}
+function glowEffect(){
+    console.log('Glow effect');
+}
+
+//--------------------------------------------------------
+// leds off
+function leds_off(){
+    console.log('Turn off all leds');
+}
+
+//--------------------------------------------------------
+// change led colour
+function changeStripColour(rgb_val){
+    console.log('Change colour to: ' + rgb_val);
+    //save value
 }
 
 var r = $('#R').slider()
